3815b98dc44b00eb127ec8ea9320011670c15535
Boryana S. Koseva <boryana@ku.edu>
2019-04-26 11:50:23 -0500

    added commands to submission script

--- scripts/align.dmel.submit.sh
+++ scripts/align.dmel.submit.sh
@@ -14,7 +14,27 @@
 module load bwa
 module load samtools
 
+# define some variables - aids in reusing a script
+MAIN_DIR=${SCRATCH}/hpc_demo
+REF_GENOME=${MAIN_DIR}/refs/dmel.chr3R.fa
+OUTPUTS_DIR=${MAIN_DIR}/outputs
+R1=${DATA_DIR}/dmel.R1.fq.gz
+R2=${DATA_DIR}/dmel.R2.fq.gz
+
 # change to the hpc_demo dir
 cd ${SCRATCH}/hpc_demo/
 
-# 
+# index the reference
+bwa index ${REF_GENOME}
+
+# align the reads
+bwa mem -t 4 ${REF_GENOME} ${R1} ${R2} > ${OUTPUTS_DIR}/dmel.aln.sam
+
+# convert SAM to BAM file
+samtools view -@ 3 -b -o ${OUTPUTS_DIR}/dmel.aln.bam ${OUTPUTS_DIR}/dmel.aln.sam
+
+# sort the BAM file
+samtools sort -@ 3 -O bam -o ${OUTPUTS_DIR}/dmel.sorted.bam ${OUTPUTS_DIR}/dmel.aln.bam
+
+# index the BAM file
+samtools index ${OUTPUTS_DIR}/dmel.sorted.bam
